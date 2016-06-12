c8ebce01fffb7686f5742fe2c5dff9510c6ffa00
David Hackro <hackro91@gmail.com>
2016-06-12 10:50:31 -0500

    Update streaming.sh

--- streaming.sh
+++ streaming.sh
@@ -7,12 +7,12 @@
      CBR="1000k" # constant bitrate (should be between 1000k - 3000k)
      QUALITY="ultrafast"  # one of the many FFMPEG preset
      AUDIO_RATE="44100"
-     STREAM_KEY=""
+     STREAM_KEY="***************************"
      #SERVER="rtmp://live.twitch.tv/app/"
      #SERVER="rtmp://live.justin.tv/app/"
-     #SERVER="rtmp://usmedia3.livecoding.tv:1935/livecodingtv/"
+     SERVER="rtmp://usmedia3.livecoding.tv:1935/livecodingtv/"
      
      ffmpeg -f x11grab -s "$INRES" -r "$FPS" -i :0.0 -f alsa -i pulse -f flv -ac 2 -ar $AUDIO_RATE \
        -vcodec libx264 -g $GOP -keyint_min $GOPMIN -b:v $CBR -minrate $CBR -maxrate $CBR -pix_fmt yuv420p\
        -s $OUTRES -preset $QUALITY -tune film -acodec libmp3lame -threads $THREADS -strict normal \
-       -bufsize $CBR "rtmp://live-fra.twitch.tv/app/"
+       -bufsize $CBR "$SERVER$STREAM_KEY"
